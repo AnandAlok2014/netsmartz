0cf1f59a785200c43d96782cccb34512ad928c48
Alok Anand <alok.anand@clsz.local>
2022-12-31 02:54:06 +0530

    Developer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './Component/Home';
 import { About } from './Component/About';
+import Contact from './Component/Contact';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <h1>Wolcome to the word of React Js</h1> 
       <Home />
       <About />
+      <Contact />
           </div>
   );
 }
